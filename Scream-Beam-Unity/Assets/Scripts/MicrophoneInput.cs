ab219c78d3955bb26a5385d0ba39ca6105256de6
Harry Wadman <harry.wadman4@gmail.com>
2018-10-21 13:46:34 +0100

    Player movement and voice input

--- Scream-Beam-Unity/Assets/Scripts/MicrophoneInput.cs
+++ Scream-Beam-Unity/Assets/Scripts/MicrophoneInput.cs
@@ -6,29 +6,32 @@ public class MicrophoneInput : MonoBehaviour {
 	
 	AudioSource audioSource;
 	
-	float updateEvery = 0.1f;
+	float updateEvery = 0.05f;
 	int sampleLength = 1024;
 	float updateTracker;
 	public float clipVolume;
 	float[] clipSample;
+	int counter;
 
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
-		audioSource.clip = Microphone.Start(null, true, 1, AudioSettings.outputSampleRate); // Start recording default mic
-		clipSample = new float[audioSource.clip.samples * audioSource.clip.channels];		// Init array for samples, clip length in sample * number of channels
+		audioSource.clip = Microphone.Start(null, true, 1, AudioSettings.outputSampleRate); 	// Start recording default mic
 	}
 	
 	void Update(){
 		
 		if(audioSource.clip != null){
-			updateTracker += Time.deltaTime;												//Run this code \/
-			if (updateTracker >= updateEvery) {												//every (updateEvery) seconds
+			updateTracker += Time.deltaTime;													//Run this code \/
+			if (updateTracker >= updateEvery) {													//every (updateEvery) seconds
+			
+				clipSample = new float[audioSource.clip.samples * audioSource.clip.channels];	// Init array for samples, clip length in sample * number of channels
+			
 				updateTracker = 0f;
-				audioSource.clip.GetData(clipSample, audioSource.timeSamples);				//sample the audio clip and store the samples in the clip sample array
-				clipVolume = 0f;															//audioSource.timeSamples refrences the current time of the audio clip
+				audioSource.clip.GetData(clipSample, audioSource.timeSamples);					//sample the audio clip and store the samples in the clip sample array
+				clipVolume = 0f;																//audioSource.timeSamples refrences the current time of the audio clip
 				foreach (var sample in clipSample) {
-					clipVolume += Mathf.Abs(sample);										//Find the average volume
-				}																			//of all samples
+					clipVolume += Mathf.Abs(sample);											//Find the average volume
+				}																				//of all samples
 				clipVolume /= sampleLength;
 			}
 		}
